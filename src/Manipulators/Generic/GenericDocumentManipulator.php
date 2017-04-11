81e84f63e3f0056170652b66c297c3f2b4bf142c
Luis Perez <lperez@combustiongroup.com>
2017-04-11 11:14:07 -0400

    Took out code that is going to be changed (commented out)

--- src/Manipulators/Generic/GenericDocumentManipulator.php
+++ src/Manipulators/Generic/GenericDocumentManipulator.php
@@ -52,30 +52,30 @@ class GenericDocumentManipulator implements Manipulator
             $thumbnail;
         }
         // create image bag and add original data
-        $imageBag = [
-            'original' => ['folder' => $path, 'name' => $name, 'extension' => $extension]
-        ];
-        $image = Image::make($path . '/' . $name . '.' . $extension);
-        if (is_array($dimensions)) {
-            $image->crop($dimensions['width'], $dimensions['height'], $dimensions['x'], $dimensions['y'])->save($path . '/' . $name . '.' . $extension);
-        }
-        foreach ($this->config['sizes'] as $size => $imageSize) {
-            // get name
-            $sizeName = md5(time() . $size . '-' . $file->getClientOriginalName());
-            // append size to the name
-            $imagePath = $path . '/' . $sizeName . '.' . $extension;
-            // make data for array
-            $imageData = [$size => ['folder' => $path, 'name' => $sizeName, 'extension' => $extension]];
-            // push data in
-            $imageBag = array_merge($imageBag, $imageData);
-            // manipulate image
-            $image->fit($imageSize['x'], $imageSize['y'], function (Constraint $constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-                // save once done
-            })->orientate()->save($imagePath);
-        }
-        return $imageBag;
+//        $imageBag = [
+//            'original' => ['folder' => $path, 'name' => $name, 'extension' => $extension]
+//        ];
+//        $image = Image::make($path . '/' . $name . '.' . $extension);
+//        if (is_array($dimensions)) {
+//            $image->crop($dimensions['width'], $dimensions['height'], $dimensions['x'], $dimensions['y'])->save($path . '/' . $name . '.' . $extension);
+//        }
+//        foreach ($this->config['sizes'] as $size => $imageSize) {
+//            // get name
+//            $sizeName = md5(time() . $size . '-' . $file->getClientOriginalName());
+//            // append size to the name
+//            $imagePath = $path . '/' . $sizeName . '.' . $extension;
+//            // make data for array
+//            $imageData = [$size => ['folder' => $path, 'name' => $sizeName, 'extension' => $extension]];
+//            // push data in
+//            $imageBag = array_merge($imageBag, $imageData);
+//            // manipulate image
+//            $image->fit($imageSize['x'], $imageSize['y'], function (Constraint $constraint) {
+//                $constraint->aspectRatio();
+//                $constraint->upsize();
+//                // save once done
+//            })->orientate()->save($imagePath);
+//        }
+//        return $imageBag;
     }
 
     private function getThumbnailFor($mimeType) : array
